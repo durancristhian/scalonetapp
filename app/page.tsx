0d994425d0369fc6d186f85a20c5f7dd2573e6ab
Cristhian Duran <durancristhian@gmail.com>
2024-08-27 00:31:31 -0300

    Clean up home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,12 @@
-import { AddPlayer } from "@/app/match-players/add-player";
-import { PlayersList } from "@/app/match-players/players-list";
 import { Footer } from "@/components/footer";
 import { Header } from "@/components/header";
 
-export default function Home() {
+export default function Page() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <div className="grow">
-        <div className="container mx-auto p-4">
-          {/* TODO: move this to a component */}
-          <div className="flex flex-col gap-4">
-            <h2 className="font-semibold text-lg">Armador de equipos</h2>
-            <div>
-              <AddPlayer />
-            </div>
-            <div>
-              <PlayersList />
-            </div>
-          </div>
-        </div>
+        <div className="container mx-auto p-4"></div>
       </div>
       <Footer />
     </div>
