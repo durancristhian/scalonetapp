c4a1a0d54b3eea8cae4c074714d17a5b5ce4ed65
Cristhian Duran <durancristhian@gmail.com>
2024-08-28 15:03:45 -0300

    Add button to create a match

--- app/matches/page.tsx
+++ app/matches/page.tsx
@@ -1,11 +1,18 @@
+import { Button } from "@/components/ui/button";
 import { MatchesList } from "./(components)/matches-list";
+import Link from "next/link";
 
 export default function Page() {
   return (
     <div className="py-8">
       <div className="container mx-auto">
         <div className="flex flex-col gap-4">
-          <h2 className="font-semibold text-2xl">Tus partidos</h2>
+          <div className="flex gap-4 items-center justify-between">
+            <h2 className="font-semibold text-2xl">Tus partidos</h2>
+            <Button asChild>
+              <Link href="/matches/new">Crear</Link>
+            </Button>
+          </div>
           <MatchesList />
         </div>
       </div>
