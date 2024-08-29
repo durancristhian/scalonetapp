8a24e185c425a018133ec6677f3cf646a1f7a8f4
Cristhian Duran <durancristhian@gmail.com>
2024-08-29 18:08:26 -0300

    Spicy tooltips

--- components/avatar.tsx
+++ components/avatar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Tooltip,
   TooltipContent,
@@ -5,7 +7,15 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip";
 import { default as BoringAvatar } from "boring-avatars";
-import { FC } from "react";
+import throttle from "lodash/throttle";
+import { FC, useState } from "react";
+
+const TOOLTIP_MESSAGES = [
+  "ee ke mirá? queré que te cague a trompada?",
+  "Te lo dije enserio!, payaso!",
+  "No bueno, me estás tomando el pelo? (Aunque no tengo hehe)",
+  "No te conozco pero dame tu url y te voy a buscar a ver si te la bancás",
+];
 
 type AvatarProps = {
   name: string;
@@ -13,17 +23,25 @@ type AvatarProps = {
 };
 
 export const Avatar: FC<AvatarProps> = ({ name, size = 48 }) => {
+  const [tooltipIndex, setTooltipIndex] = useState(-1);
+
+  const handleMouseEnter = throttle(() => {
+    setTooltipIndex(
+      tooltipIndex + 1 === TOOLTIP_MESSAGES.length ? 0 : tooltipIndex + 1
+    );
+  }, 1000);
+
   return (
     <TooltipProvider>
       <Tooltip>
         <TooltipTrigger asChild>
-          {/* This div here is necessary since the BoringAvatar component doesn't spread the ref. */}
-          <div>
+          {/* This div here is necessary since the BoringAvatar component doesn't spread the ref AND we need an element to attach an event listener so we can display different tooltip messages */}
+          <div onMouseEnter={handleMouseEnter}>
             <BoringAvatar name={name} variant="beam" size={size} />
           </div>
         </TooltipTrigger>
         <TooltipContent>
-          <p>ee ke mirá? queré que te cague a trompada?</p>
+          <p>{TOOLTIP_MESSAGES[tooltipIndex]}</p>
         </TooltipContent>
       </Tooltip>
     </TooltipProvider>
