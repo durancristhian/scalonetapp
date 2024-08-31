f240ca22f736eeefbe36eca426138e6fe41aa568
Cristhian Duran <durancristhian@gmail.com>
2024-08-31 16:15:59 -0300

    Add confirmation to delete matches

--- app/(components)/match-item.tsx
+++ app/(components)/match-item.tsx
@@ -1,5 +1,16 @@
 "use client";
 
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
 import { Match } from "@prisma/client";
 import { ArrowRightIcon, BugIcon, TrashIcon } from "lucide-react";
@@ -34,11 +45,29 @@ export const MatchItem: FC<MatchItemProps> = ({ deleteMatch, match }) => {
 
   return (
     <div className="flex items-center gap-2">
-      <form action={onDeleteMatch}>
-        <Button type="submit" variant="ghost" size="icon">
-          <TrashIcon className="h-4 text-red-700 w-4" />
-        </Button>
-      </form>
+      <AlertDialog>
+        <AlertDialogTrigger asChild>
+          <Button variant="ghost" size="icon">
+            <TrashIcon className="h-4 text-red-700 w-4" />
+          </Button>
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>
+              ¿Estás seguro de eliminar este partido?
+            </AlertDialogTitle>
+            <AlertDialogDescription>
+              Esta acción no se puede deshacer.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancelar</AlertDialogCancel>
+            <AlertDialogAction asChild>
+              <Button onClick={onDeleteMatch}>Eliminar</Button>
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
       <div className="grow">
         <Link href={`/${match.id}`}>
           <div className="border border-slate-300 hover:bg-slate-50 px-4 py-2 rounded-md transition-colors">
