10231e95063b69874ee23696753709cf94b0bd55
Cristhian Duran <durancristhian@gmail.com>
2024-09-03 10:53:25 -0300

    Last updated match description

--- app/dashboard/(components)/match-item.tsx
+++ app/dashboard/(components)/match-item.tsx
@@ -3,6 +3,8 @@
 import { DeleteMatch } from "@/app/dashboard/(components)/delete-match";
 import { EditMatch } from "@/app/dashboard/(components)/edit-match";
 import { Match } from "@prisma/client";
+import { formatDistanceToNow } from "date-fns";
+import { es } from "date-fns/locale/es";
 import { ArrowRightIcon } from "lucide-react";
 import Link from "next/link";
 import { FC } from "react";
@@ -20,7 +22,15 @@ export const MatchItem: FC<MatchItemProps> = ({ match }) => {
         <Link href={`/matches/${match.id}`}>
           <div className="border border-slate-300 hover:bg-slate-50 px-4 py-2 rounded-md transition-colors">
             <div className="flex items-center justify-between gap-4">
-              <p>{match.name}</p>
+              <div>
+                <p>{match.name}</p>
+                <p className="text-slate-500 text-sm">
+                  Última actualización:&nbsp;
+                  {formatDistanceToNow(match.updatedAt, {
+                    locale: es,
+                  })}
+                </p>
+              </div>
               <ArrowRightIcon className="h-4 text-slate-500 w-4" />
             </div>
           </div>
