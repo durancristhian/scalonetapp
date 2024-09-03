b8fc5fe2a5414eed1139a4c6b31041db0339f94e
Cristhian Duran <durancristhian@gmail.com>
2024-09-03 18:12:04 -0300

    Update in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,9 @@ import type { Config } from "tailwindcss";
 const config = {
   content: ["./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}"],
   darkMode: ["class"],
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
   plugins: [require("tailwindcss-animate")],
 } satisfies Config;
 
