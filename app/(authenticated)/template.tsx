0be6a90f10b132c32fe84d521adef85e669a6487
Cristhian Duran <durancristhian@gmail.com>
2024-09-25 11:33:57 -0300

    Update template

--- app/(authenticated)/template.tsx
+++ app/(authenticated)/template.tsx
@@ -6,8 +6,8 @@ import { FC, PropsWithChildren } from "react";
 const Template: FC<PropsWithChildren> = ({ children }) => {
   return (
     <motion.div
-      initial={{ opacity: 0, y: 16 }}
-      animate={{ opacity: 1, y: 0 }}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
       transition={{ ease: "easeInOut", duration: 0.5 }}
     >
       {children}
