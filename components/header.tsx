a5e9f68158b092e97039e55596165a9829453a6f
Cristhian Duran <durancristhian@gmail.com>
2024-09-25 10:32:26 -0300

    Nice header

--- components/header.tsx
+++ components/header.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 
 export const Header = () => {
   return (
-    <div className="bg-white border-b border-border">
+    <div className="backdrop-blur-sm bg-white/75 border-b border-border sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-4 w-full">
         <div className="flex gap-4 h-12 md:h-14 items-center justify-between">
           <div className="flex gap-2 items-center">
